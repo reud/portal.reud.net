4cc00118878ddc6cbc02c7875b4e2fc903df1e2e
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2021-02-20 11:40:39 +0900

    add link

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -4,6 +4,7 @@
       <v-chip
         class="ma-2"
         label
+        color="grey"
         href="https://atcoder.jp/users/reud"
       >
         <v-icon left>
@@ -15,12 +16,37 @@
         class="ma-2"
         label
         link
+        color="orange"
         href="https://blog.reud.net/"
       >
         <v-icon left>
           mdi-account-circle
         </v-icon>
         Blog
+      </v-chip>
+      <v-chip
+        class="ma-2"
+        label
+        link
+        color="green"
+        href="https://qiita.com/reud"
+      >
+        <v-icon left>
+          mdi-account-circle
+        </v-icon>
+        Qiita
+      </v-chip>
+      <v-chip
+        class="ma-2"
+        label
+        link
+        color="light-blue"
+        href="https://zenn.dev/ac"
+      >
+        <v-icon left>
+          mdi-account-circle
+        </v-icon>
+        Zenn
       </v-chip>
     </v-row>
   </v-container>
